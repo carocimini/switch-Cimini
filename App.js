77f38bcc73123fbc1ebd54c512ec8acde989877a
Carola Cimini <caro.cimini@gmail.com>
2022-11-15 21:25:56 -0300

    se agregaron Fonts y libreria

--- App.js
+++ App.js
@@ -7,9 +7,14 @@ import Header from "./components/Header";
 import ListScreen from "./screens/listScreen";
 import StartAppScreen from "./screens/StartAppScreen";
 import colors from "./constants/colors";
+import { useFonts } from "expo-font";
 import { useState } from "react";
 
 export default function App() {
+const [loaded] = useFonts({
+  PermanentMarker: require('./assets/fonts/PermanentMarker-Regular.ttf')
+})
+  
   const [listName, setListName] = useState()
 
   const handleStartList = (selectedName) => {
@@ -20,10 +25,14 @@ export default function App() {
   if (listName) {
     content = <ListScreen/>
   }
+
+  if(!loaded) {
+    return null
+  }
   
   return (
     <View style={styles.container}>
-      <Header title={"TodayDo"}/>
+      <Header title={"TodayDo"} newStyles={{fontFamily: 'PermanentMarker'}}/>
       {content}
     </View>
   )
